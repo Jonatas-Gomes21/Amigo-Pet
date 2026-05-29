16adc0ed73d9af0ffa6257156ec819dabc91226b
JonatasGomes <jonatasgomeslima21@gmail.com>
2026-05-28 21:28:34 -0300

    feat: Criação de uma nova section Nossa Missão, adição do componente Scroll progress ao projeto e organização do codigo para melhor entendimento

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,18 +4,30 @@ import { AuroraText } from "@/components/ui/aurora-text";
 import Buttonsmenu from "../../components/Buttonsmenu";
 import Buttonsoutline from "../../components/Buttonsoutline";
 import { Link } from "react-router-dom";
-import { ArrowRight, Wheat, BriefcaseMedical, ShieldCheck, Droplets, CheckCircle2, Stethoscope, House } from "lucide-react";
+import {
+  ArrowRight,
+  Wheat,
+  BriefcaseMedical,
+  ShieldCheck,
+  Droplets,
+  CheckCircle2,
+  Stethoscope,
+  House,
+  Sprout,
+} from "lucide-react";
 import DogImage from "../../assets/Dogs.png";
 import BanhoImage from "../../assets/Banho.png";
 import Clinica from "../../assets/Clinica.png";
 import Hotel from "../../assets/Hotel.png";
+import { ScrollProgress } from "../../components/ui/scroll-progress";
 
 function Home() {
-  const diferencialCardClass = "flex flex-col items-center md:items-start text-center md:text-left gap-4 p-4 md:p-8 shadow-xl rounded-xl bg-white";
+  const diferencialCardClass =
+    "flex flex-col items-center md:items-start text-center md:text-left gap-4 p-4 md:p-8 shadow-xl rounded-xl bg-white";
 
   return (
     <main className="w-full bg-white">
-      
+      <ScrollProgress className="top-0 py-0.5" />
       <div className="w-full min-h-screen flex flex-col md:flex-row justify-between px-6 py-10 md:px-16 md:pt-12 pb-28 gap-12 overflow-hidden">
         <motion.section
           initial={{ opacity: 0, x: -30 }}
@@ -40,9 +52,9 @@ function Home() {
             </div>
 
             <p className="text-stone-600 text-center md:text-start text-base md:text-lg font-normal font-['Hanken_Grotesk'] leading-relaxed max-w-md md:max-w-xl">
-              No Amigo Verde, combinamos amor, natureza e excelência para
-              oferecer o melhor em saúde e bem-estar animal. Uma experiência
-              premium focada na vitalidade do seu pet.
+              No <strong>Amigo Pet</strong>, combinamos amor, natureza e
+              excelência para oferecer o melhor em saúde e bem-estar animal. Uma
+              experiência premium focada na vitalidade do seu pet.
             </p>
 
             <div className="w-full flex flex-col sm:flex-row justify-center gap-3 md:justify-start md:gap-4">
@@ -88,7 +100,7 @@ function Home() {
             className="w-full flex text-center flex-col justify-center items-center gap-3"
           >
             <h2 className="text-stone-900 text-3xl font-semibold font-['Quicksand'] md:text-4xl">
-              A Diferença Amigo Verde
+              A Diferença Amigo Pet
             </h2>
             <p className="max-w-2xl text-base md:text-lg text-stone-600 font-normal font-['Hanken_Grotesk'] leading-relaxed">
               Nossos pilares de cuidado garantem que seu pet receba não apenas
@@ -165,11 +177,10 @@ function Home() {
           </div>
         </div>
       </section>
-      
+
       <section className="w-full bg-[#F5F3EE] px-6 py-16 md:px-16 md:py-28 flex flex-col justify-center items-center overflow-hidden">
         <div className="w-full max-w-6xl flex flex-col gap-12 md:gap-16">
-          
-          <motion.div 
+          <motion.div
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.5 }}
@@ -181,14 +192,14 @@ function Home() {
                 Nossos Serviços
               </h2>
               <p className="text-stone-700 text-base md:text-lg font-normal font-['Hanken_Grotesk'] leading-relaxed">
-                Cuidado integral em um só lugar. Cada serviço é realizado com foco na saúde orgânica e emocional do seu companheiro.
+                Cuidado integral em um só lugar. Cada serviço é realizado com
+                foco na saúde orgânica e emocional do seu companheiro.
               </p>
             </div>
           </motion.div>
 
           <div className="grid grid-cols-1 md:grid-cols-1 gap-12 w-full mt-12">
-            
-            <motion.article 
+            <motion.article
               initial={{ opacity: 0, y: 40 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.2 }}
@@ -197,10 +208,10 @@ function Home() {
             >
               <div className="w-full md:w-3/5 p-4 md:p-6">
                 <div className="w-full h-80 md:h-112.5 overflow-hidden rounded-2xl border-2 border-dashed border-blue-400 p-2 bg-stone-50">
-                  <img 
-                    src={BanhoImage} 
-                    alt="Veterinária banhando um cachorro Golden Retriever" 
-                    className="w-full h-full object-cover rounded-xl" 
+                  <img
+                    src={BanhoImage}
+                    alt="Veterinária banhando um cachorro Golden Retriever"
+                    className="w-full h-full object-cover rounded-xl"
                   />
                 </div>
               </div>
@@ -220,9 +231,10 @@ function Home() {
                 </div>
 
                 <p className="text-stone-700 text-sm font-normal font-['Hanken_Grotesk'] leading-relaxed max-w-xl">
-                  Transforme o banho em um momento de relaxamento. Utilizamos xampus à
-                  base de plantas, ozonioterapia e técnicas de massagem relaxante para uma
-                  pelagem brilhante e pele saudável, sem estresse.
+                  Transforme o banho em um momento de relaxamento. Utilizamos
+                  xampus à base de plantas, ozonioterapia e técnicas de massagem
+                  relaxante para uma pelagem brilhante e pele saudável, sem
+                  estresse.
                 </p>
 
                 <ul className="space-y-4">
@@ -242,15 +254,15 @@ function Home() {
 
                 <div className="mt-4">
                   <Link to={"https://w.app/amigoverde"}>
-                  <button className="inline-flex items-center justify-center border border-[#1A2E20] text-[#1A2E20] hover:bg-[#1A2E20] hover:text-white rounded-full px-7 py-3 text-sm font-semibold font-['Hanken_Grotesk'] transition-colors duration-300 cursor-pointer">
-                    Agendar Spa
-                  </button>
+                    <button className="inline-flex items-center justify-center border border-[#1A2E20] text-[#1A2E20] hover:bg-[#1A2E20] hover:text-white rounded-full px-7 py-3 text-sm font-semibold font-['Hanken_Grotesk'] transition-colors duration-300 cursor-pointer">
+                      Agendar Spa
+                    </button>
                   </Link>
                 </div>
               </div>
             </motion.article>
 
-            <motion.article 
+            <motion.article
               initial={{ opacity: 0, y: 40 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.2 }}
@@ -272,9 +284,10 @@ function Home() {
                 </div>
 
                 <p className="text-stone-700 text-sm font-normal font-['Hanken_Grotesk'] leading-relaxed max-w-xl">
-                  Consultas especializadas que unem o melhor da medicina veterinária
-                  moderna com abordagens naturais. Prevenção, nutrição clínica e diagnóstico
-                  preciso em um ambiente que transmite calma.
+                  Consultas especializadas que unem o melhor da medicina
+                  veterinária moderna com abordagens naturais. Prevenção,
+                  nutrição clínica e diagnóstico preciso em um ambiente que
+                  transmite calma.
                 </p>
 
                 <ul className="space-y-4">
@@ -300,16 +313,16 @@ function Home() {
               </div>
               <div className="w-full md:w-3/5 p-4 md:p-6 order-1 md:order-2">
                 <div className="w-full h-80 md:h-112.5 overflow-hidden rounded-2xl border-2 border-dashed border-blue-400 p-2 bg-stone-50">
-                  <img 
-                    src={Clinica} 
-                    alt="Veterinário examinando um pet com cuidado" 
-                    className="w-full h-full object-cover rounded-xl" 
+                  <img
+                    src={Clinica}
+                    alt="Veterinário examinando um pet com cuidado"
+                    className="w-full h-full object-cover rounded-xl"
                   />
                 </div>
               </div>
             </motion.article>
 
-            <motion.article 
+            <motion.article
               initial={{ opacity: 0, y: 40 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true, amount: 0.2 }}
@@ -318,10 +331,10 @@ function Home() {
             >
               <div className="w-full md:w-3/5 p-4 md:p-6">
                 <div className="w-full h-80 md:h-112.5 overflow-hidden rounded-2xl border-2 border-dashed border-blue-400 p-2 bg-stone-50">
-                  <img 
-                    src={Hotel} 
-                    alt="Cachorros socializando felizes na creche" 
-                    className="w-full h-full object-cover rounded-xl" 
+                  <img
+                    src={Hotel}
+                    alt="Cachorros socializando felizes na creche"
+                    className="w-full h-full object-cover rounded-xl"
                   />
                 </div>
               </div>
@@ -341,9 +354,10 @@ function Home() {
                 </div>
 
                 <p className="text-stone-700 text-sm font-normal font-['Hanken_Grotesk'] leading-relaxed max-w-xl">
-                  Um espaço de socialização desenhado para o bem-estar mental e físico do
-                  seu pet. Áreas separadas por porte e temperamento, com recreação
-                  monitorada por especialistas em comportamento animal.
+                  Um espaço de socialização desenhado para o bem-estar mental e
+                  físico do seu pet. Áreas separadas por porte e temperamento,
+                  com recreação monitorada por especialistas em comportamento
+                  animal.
                 </p>
 
                 <ul className="space-y-4">
@@ -372,7 +386,34 @@ function Home() {
         </div>
       </section>
 
-
+      <section className="w-full flex justify-center items-center px-6 py-20 md:py-28 bg-green-300/20">
+        <motion.div 
+          initial={{ opacity: 0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true, amount: 0.4 }}
+          transition={{ duration: 0.7, ease: "easeOut" }}
+          className="w-full max-w-4xl flex flex-col justify-center items-center bg-white rounded-3xl shadow-[0px_10px_40px_-10px_rgba(45,90,39,0.08)] p-8 md:p-16 gap-6"
+        >
+          <Sprout className="size-8 text-[#154212]" />
+          
+          <h2 className="text-center text-stone-900 text-3xl md:text-4xl font-semibold font-['Quicksand'] leading-tight max-w-lg">
+            Nossa Missão: Reconectar Pets à Natureza
+          </h2>
+          
+          <div className="w-full flex justify-center items-center mt-2">
+            <p className="text-center text-stone-700 text-base md:text-lg font-normal font-['Hanken_Grotesk'] leading-relaxed max-w-2xl">
+              Acreditamos que nossos animais de estimação merecem o mesmo nível de
+              cuidado orgânico e saudável que buscamos para nós mesmos. O Amigo
+              Verde nasceu do desejo de criar um refúgio de bem-estar onde a
+              biologia natural do animal é respeitada e celebrada em cada serviço
+              que prestamos.
+            </p>
+          </div>
+          <div className="w-full h-62.5 sm:h-87.5 md:h-105 flex justify-center items-center mt-4">
+            <img src={DogImage} className="w-full h-full object-cover rounded-2xl" alt="" />
+          </div>
+        </motion.div>
+      </section>
     </main>
   );
 }
