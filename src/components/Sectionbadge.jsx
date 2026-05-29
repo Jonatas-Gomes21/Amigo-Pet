e9737bf019611c034c888963b39c2fe2c92ea665
JonatasGomes <jonatasgomeslima21@gmail.com>
2026-05-29 20:19:36 -0300

    chore: melhora na organização do codigo

--- src/components/Sectionbadge.jsx
+++ src/components/Sectionbadge.jsx
@@ -3,7 +3,7 @@ import { Badge } from "./ui/badge";
 function Sectionbadge() {
   return (
     <Badge variant="outline" className="bg-[#E4E2DD] border-[#4B654233]">
-      <span className=" text-stone-700 text-xs font-normal font-['Hanken_Grotesk']" >
+      <span className=" text-stone-700 text-xs font-normal font-['Hanken_Grotesk']">
         Spa Premium
       </span>
     </Badge>
