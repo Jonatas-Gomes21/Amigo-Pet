32d3553d3dc86c2e184e383ed426cc6d404dfab9
JonatasGomes <jonatasgomeslima21@gmail.com>
2026-06-02 23:15:49 -0300

    refactor:  Adição do lazy e do suspense para melhor desenpenho das rotas.

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footers from "./components/Footers";
-import Home from "./pages/Home";
-import SobreNos from "./pages/SobreNos";
-import Agendamento from "./pages/Agendamento";
-import Servicos from "./pages/Servicos";
 import ScrollToTop from "./components/ScrollToTop";
+
+const Home = lazy(() => import("./pages/Home"));
+const SobreNos = lazy(() => import("./pages/SobreNos"));
+const Agendamento = lazy(() => import("./pages/Agendamento"));
+const Servicos = lazy(() => import("./pages/Servicos"));
 
 function RouterApp() {
   return (
@@ -13,12 +15,15 @@ function RouterApp() {
       <ScrollToTop />
       <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/servicos" element={<Servicos />} />
-        <Route path="/sobrenos" element={<SobreNos />} />
-        <Route path="/agendamento" element={<Agendamento />} />
-      </Routes>
+      <Suspense fallback={<div className="w-full min-h-screen bg-white" />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/servicos" element={<Servicos />} />
+          <Route path="/sobrenos" element={<SobreNos />} />
+          <Route path="/agendamento" element={<Agendamento />} />
+        </Routes>
+      </Suspense>
+
       <Footers />
     </BrowserRouter>
   );
