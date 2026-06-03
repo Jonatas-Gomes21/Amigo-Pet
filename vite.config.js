5d1050e1caea370e03b75de002908b39102b9b01
JonatasGomes <jonatasgomeslima21@gmail.com>
2026-06-02 23:24:57 -0300

    fix: remoção do plugin

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import flowbiteReact from "flowbite-react/plugin/vite";
-import sitemap from "vite-plugin-sitemap"; // ← adiciona essa linha
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     flowbiteReact(),
-    sitemap({
-      hostname: "https://amigo-petshop.vercel.app",
-      routes: ["/", "/agendamento"],
-    }),
   ],
   resolve: {
     alias: {
