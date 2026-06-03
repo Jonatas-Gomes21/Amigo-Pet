bb946c3bf06e81b0b95ed5fab716699663e8948c
JonatasGomes <jonatasgomeslima21@gmail.com>
2026-06-02 22:36:17 -0300

    Feat: Criação da nova rota para a page de agendamento de serviços e adição do scrollToTop

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,16 +3,20 @@ import Header from "./components/Header";
 import Footers from "./components/Footers";
 import Home from "./pages/Home";
 import SobreNos from "./pages/SobreNos";
+import Agendamento from "./pages/Agendamento";
 import Servicos from "./pages/Servicos";
+import ScrollToTop from "./components/ScrollToTop";
 function RouterApp() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Header />
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/servicos" element={<Servicos />} />
         <Route path="/sobrenos" element={<SobreNos />} />
+        <Route path="/agendamento" element={<Agendamento />} />
       </Routes>
       <Footers />
     </BrowserRouter>
